b31b9bcbef300473da52b0db85c547e569a56cd2
Ze Diogo <up202003529@edu.fe.up.pt>
2022-03-31 21:28:34 +0100

    Added file readers for both files, still need to test them

---
+++ include/FileReader.h
@@ -0,0 +1,22 @@
+//
+// Created by zediogo96 on 31/03/2022.
+//
+
+#ifndef PROJETO1_FILEREADER_H
+#define PROJETO1_FILEREADER_H
+
+#include <iostream>
+#include <vector>
+
+#include "DeliveryMan.h"
+#include "Package.h"
+
+using namespace std;
+
+vector<string> fileReader(const string& f_name);
+
+vector<DeliveryMan> *getDeliveryManFromFiles(string f1);
+
+vector<Package> *getPackagesFromFiles(const string& f2);
+
+#endif //PROJETO1_FILEREADER_H
---
+++ src/FileReader.cpp
@@ -0,0 +1,70 @@
+//
+// Created by zediogo96 on 31/03/2022.
+//
+
+#include "../include/FileReader.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+vector<string> fileReader(const string& f_name) {
+
+    ifstream inFile;
+    string line;
+
+    vector<string> v1;
+
+    inFile.open(f_name);
+
+    if (inFile.is_open())  {
+        /** IGNORE FIRST LINE, DESCRIPTION OF THE COLUMNS **/
+        getline(inFile, line);
+
+        while (getline(inFile, line)) {
+            v1.emplace_back(line);
+        }
+        inFile.close();
+    }
+    return v1;
+}
+
+vector<DeliveryMan> *getDeliveryManFromFiles(string f1) {
+
+    vector<DeliveryMan> * deliveryman = new vector<DeliveryMan>;
+    vector<string> inputsFromFile;
+
+    unsigned maxV, maxW, cost;
+
+    inputsFromFile = fileReader(f1);
+
+    for (const auto& line : inputsFromFile) {
+        stringstream ss(line);
+        ss >> maxV;
+        ss >> maxW;
+        ss >> cost;
+
+        deliveryman->emplace_back(DeliveryMan(maxV, maxW, cost));
+    }
+    return deliveryman;
+}
+
+vector<Package> *getPackagesFromFiles(const string& f2) {
+    vector<Package> * packages = new vector<Package>;
+    vector<string> inputsFromFile;
+
+    unsigned volume, weight, reward, duration;
+
+    inputsFromFile = fileReader(f2);
+
+    for (const auto& line : inputsFromFile) {
+        stringstream ss(line);
+        ss >> volume;
+        ss >> weight;
+        ss >> reward;
+        ss >> duration;
+
+        packages->emplace_back(Package(volume, weight, reward, duration));
+    }
+    return packages;
+}
