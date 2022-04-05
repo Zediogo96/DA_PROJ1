b59e7db382bbbb93396aed6f1bab4a4fa1f760e2
Ze Diogo <up202003529@edu.fe.up.pt>
2022-04-05 15:54:10 +0100

    Scenery 3 probable solution

--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
-#include <iostream>
-
 #include "include/FileReader.h"
 
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+
 using namespace std;
+
+int scenery3(const vector<Package>& packages) {
+
+    vector<Package> auxVec;
+    vector<Package> expressPackages;
+
+    auxVec.reserve(packages.size());
+
+    for (const Package& pack : packages) {
+        auxVec.emplace_back(pack);
+    }
+
+    sort(auxVec.begin(), auxVec.end(), [](Package & a,Package & b) {
+        return a.getDuration() < b.getDuration();
+    });
+
+    unsigned timeLeft = 8 * 3600;
+
+    for (Package aPackage : auxVec) {
+        if (aPackage.getDuration() <= timeLeft) {
+            expressPackages.push_back(aPackage);
+            timeLeft -= aPackage.getDuration();
+        }
+        else break;
+    }
+
+    return (int)(((8 * 3600) - timeLeft) / expressPackages.size());
+}
 
 int main() {
 
     vector<Package> * test;
     test = getPackagesFromFiles("../input/package.txt");
-
-    for (auto tmp : *test) {
-        cout << tmp.getVolume() << " " << tmp.getWeight() << " " << tmp.getReward() << " " << tmp.getDuration() << endl;
-    }
-    return 0;
+    cout << scenery3(*test);
 }
